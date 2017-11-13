b8bed5ec1880dae2eea6152e00015143c802c99d
Tehnikumi Õpilane <opilane@tptlive.ee>
2017-11-13 15:50:33 +0200

    Koolis

---
+++ scr/05_schoolwork/04_Excercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_Excercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vabanduste genereerims programm");
+            Console.WriteLine("Sisesta komadega eraldarud vabandused:");
+            Console.Write(">");
+            
+            
+
+        }
+    }
+}
